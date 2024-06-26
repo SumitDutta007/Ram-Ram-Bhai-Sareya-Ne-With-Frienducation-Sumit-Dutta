4b56a3ea31eb0fa40baa4f09eebee091cf107957
Sumit Dutta <sumitdutta2810@gmail.com>
2024-06-26 18:13:53 +0530

    Add files via upload

---
+++ Remove Duplicates from Sorted Array.cpp
@@ -0,0 +1,11 @@
+int removeDuplicates(vector<int>& nums) {
+        int k =1, n=nums.size();
+        for(int i=1 ; i<n ; i++){
+            if(nums[i] != nums[i-1]){
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+
+    }
