fa18cac0dea404db72b2c821c2847b410fcc1f90
Sumit Dutta <sumitdutta2810@gmail.com>
2024-06-27 18:29:20 +0530

    Add files via upload

---
+++ Majority Element.cpp
@@ -0,0 +1,15 @@
+int majorityElement(vector<int>& nums) {
+        int count = 1;
+        int majority = nums[0];
+        for(int i=1 ; i<nums.size() ; i++){
+            if(nums[i]==majority) count++;
+            else{
+                count--;
+                if(count == 0){
+                    majority = nums[i];
+                    count++;
+                }
+            }
+        }
+        return majority;
+    }
